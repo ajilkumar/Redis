52181edee77691dc96ff99eb19429b68e4420a64
ajilkumar <ajilk0325@gmail.com>
2026-01-28 19:55:04 +0530

    Minor updates

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ import redisClient from "./client.js";
 const app = express();
 
 app.get("/", async (_req, res) => {
+
+    // Check if data is present in Redis cache
   const cachedValues = await redisClient.get("todos");
 
   if (cachedValues) {
     return res.json(JSON.parse(cachedValues));
   }
 
+    // Fetch data from external API if not present in cache   
   const { data } = await axios.get(
     "https://jsonplaceholder.typicode.com/todos",
   );
@@ -19,6 +22,7 @@ app.get("/", async (_req, res) => {
   return res.json(data);
 });
 
+// Start the server
 app.listen(9000, () => {
   console.log(`Server is up and running on Port 9000`);
 });
